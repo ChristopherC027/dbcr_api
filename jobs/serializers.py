c911da71424c243a391e1006f3f15015f5504858
Christopher Castillo <christopherhcastillo@gmail.com>
2021-01-04 11:10:13 -0500

    Update serializers.py

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -6,8 +6,9 @@ from .models import Job
 class JobSerializer(serializers.ModelSerializer):
     posted_at = serializers.DateField(format="%m-%d-%Y")
     expires_at = serializers.DateField(format="%m-%d-%Y")
+    id = serializers.ReadOnlyField()
 
     class Meta:
 
-        fields = ('company', 'title', 'posted_at', 'expires_at', 'website', 'tag')
+        fields = ('id', 'company', 'title', 'posted_at', 'expires_at', 'website', 'tag')
         model = Job
